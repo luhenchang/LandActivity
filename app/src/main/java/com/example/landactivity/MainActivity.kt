99d2ca9cd122b12685de801c8e7ff5e03599a787
wangfei44 <wangfei44@lenovo.com>
2022-07-05 14:59:41 +0800

    给framwork写了个zuilib的弹窗动画。

--- app/src/main/java/com/example/landactivity/MainActivity.kt
+++ app/src/main/java/com/example/landactivity/MainActivity.kt
@@ -1,33 +1,65 @@
 package com.example.landactivity
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
+import android.widget.CompoundButton
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import zui.app.MessageDialog
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var helloView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val orientation = resources.configuration.orientation
-        Log.e(
-            "MainActivity",
-            "current screen is " + if (orientation == 1) "portrait" else " landscape "
-        )
-        setContentView(R.layout.activity_screen_orientation)
         setContentView(R.layout.activity_main)
-        helloView = findViewById<TextView>(R.id.helloId)
+        showPowerDialogIfNeeded()
     }
 
-    fun goToScreenActivity(view: View) {
-        helloView.postDelayed({
-            val fullScreenIntent = Intent(this, ScreenOrientationActivity::class.java)
-            fullScreenIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(fullScreenIntent)
-        }, 4000)
 
+
+    //需要获取访问本地录音权限的dialog
+    private fun showPowerDialogIfNeeded() {
+        val checkedChangeListener =
+            CompoundButton.OnCheckedChangeListener { buttonView, isChecked -> {
+
+            }}
+        val listener =
+            DialogInterface.OnClickListener { dialog, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+
+                    }
+                    DialogInterface.BUTTON_NEGATIVE -> {
+                        finish()
+                    }
+                }
+            }
+        val builder: MessageDialog.Builder = MessageDialog.Builder(this)
+            .setTitle("录音机权限")
+            .setMessageDialogType(MessageDialog.TYPE_PERMISSION)
+            .setCheckMessage("再次统一么？", true)
+            .setOnCheckedChangeListener(checkedChangeListener)
+            .addGroup("网络权限统一福", 0)
+            .setPositiveButton("同意", listener)
+            .setNegativeButton("取消", listener)
+        val titles = resources.getStringArray(R.array.cta_items_title)
+        val msgs = resources.getStringArray(R.array.cta_items_msg)
+        for (i in titles.indices) {
+            builder.addPairedItem2Group(
+                0,
+                titles[i],
+                msgs[i],
+                getDrawable(R.drawable.ic_launcher_foreground)
+            )
+        }
+        val dialog: MessageDialog = builder.create()
+        dialog.setCancelableOnOrientation(false)
+        dialog.setOnKeyListener { _, keyCode, event -> keyCode === KeyEvent.KEYCODE_BACK }
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/landactivity/ScreenOrientationActivity.kt
+++ app/src/main/java/com/example/landactivity/ScreenOrientationActivity.kt
@@ -1,20 +1,37 @@
 package com.example.landactivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
-import android.util.Log.d
+import android.view.WindowManager
 import android.widget.TextView
-import java.util.logging.Logger
+import androidx.appcompat.app.AppCompatActivity
 
 class ScreenOrientationActivity : AppCompatActivity() {
     val TAG  = this.javaClass.name
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val orientation = resources.configuration.orientation
-        Log.e(TAG,"current screen is " + if (orientation == 1) "portrait" else " landscape ")
+        Log.e(TAG, "onCreate:::")
+        val win = window
+        win.addFlags(
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+        )
         setContentView(R.layout.activity_screen_orientation)
         val result = findViewById<TextView>(R.id.result)
+        val orientation = resources.configuration.orientation
+        Log.e(TAG, "current screen orientation = $orientation")
+        Log.e(TAG, "current screen is " + if (orientation == 1) "portrait" else " landscape ")
+        val currentOrientation =
+            if (orientation == 1) ActivityInfo.SCREEN_ORIENTATION_PORTRAIT else ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        requestedOrientation = currentOrientation
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy:::")
     }
 }
